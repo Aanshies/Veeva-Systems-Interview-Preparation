46e4b9b6b1f9b5890db29d43e8722547efe0d496
Eswari <158579538+Aanshies@users.noreply.github.com>
2025-12-28 16:04:32 +0530

    Update SquareRootArithmeticException.java

--- java-basic/Exceptions/SquareRootArithmeticException.java
+++ java-basic/Exceptions/SquareRootArithmeticException.java
@@ -1,19 +1,17 @@
-class SquareRootArithmeticException {
-
-    public static void main(String[] args) {
-
-        int number = -5;
-
-        try {
-            if (number < 0) {
-                throw new ArithmeticException("No negatives!");
+class Main{
+    public static void main(String[] args){
+        int number=5;
+        try{
+            if(number<0){
+                throw new ArithmeticException("No negatives!");//IllegealArgumentException
             }
-
-            double result = Math.sqrt(number);
-            System.out.println("Square root: " + result);
-
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception: " + e.getMessage());
+            else{
+                double res=Math.sqrt(number);
+                System.out.println("Square root of "+number+" is "+res);
+            }
+            }
+            catch(ArithmeticException e){
+                System.out.println("Arithmetic Exception:"+e.getMessage());
+            }
         }
     }
-}
